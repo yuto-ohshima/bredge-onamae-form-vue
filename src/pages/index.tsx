46cb7b4cf66f0e85af2a6d6f6731b9cef1b94022
YutoOhshima <47956167+yuto-ohshima@users.noreply.github.com>
2024-07-10 20:32:28 +0900

    modify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,58 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { OnamaeForm } from "onamae-form";
-import { useCallback, useEffect, useRef, useState } from "react";
-import { App, createApp, h } from "vue";
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { OnamaeForm } from 'onamae-form'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { App, createApp, h } from 'vue'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [onamae, setOnamae] = useState<{ firstName: string; lastName: string }>(
-    {
-      firstName: "firstName",
-      lastName: "lastName",
-    },
-  );
+  const [onamae, setOnamae] = useState<{ firstName: string; lastName: string }>({
+    firstName: '',
+    lastName: ''
+  })
 
   const onChangeFirstName = useCallback((value: string) => {
-    setOnamae((prev) => ({ ...prev, firstName: value }));
-  }, []);
+    setOnamae((prev) => ({ ...prev, firstName: value }))
+  }, [])
   const onChangeLastName = useCallback((value: string) => {
-    setOnamae((prev) => ({ ...prev, lastName: value }));
-  }, []);
-
-  const rootForVue = useRef<HTMLDivElement | null>(null);
+    setOnamae((prev) => ({ ...prev, lastName: value }))
+  }, [])
 
   const onamaeFormVuePropsRef = useRef({
     firstName: onamae.firstName,
     lastName: onamae.lastName,
     onChangeFirstName,
-    onChangeLastName,
-  });
+    onChangeLastName
+  })
 
   useEffect(() => {
-    let onamaeFormVue: App<Element> | null = null;
-    if (!onamaeFormVue && rootForVue.current) {
+    let onamaeFormVue: App<Element> | null = null
+    if (!onamaeFormVue) {
       onamaeFormVue = createApp({
         setup() {
           return () => {
-            const _h = h(OnamaeForm);
+            const _h = h(OnamaeForm)
             _h.props = {
-              ...onamaeFormVuePropsRef.current,
-            };
+              ...onamaeFormVuePropsRef.current
+            }
 
-            return _h;
-          };
-        },
-      });
-      onamaeFormVue.mount(rootForVue.current);
+            return _h
+          }
+        }
+      })
+      onamaeFormVue.mount('#onamae-form')
     }
 
     return () => {
       if (onamaeFormVue) {
-        onamaeFormVue.unmount();
+        onamaeFormVue.unmount()
       }
-    };
-  }, []);
+    }
+  }, [])
 
   return (
-    <main
-      className={`flex gap-y-6 flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <main className={`flex gap-y-6 flex-col items-center justify-between p-24 ${inter.className}`}>
       <Image
         className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
         src="/next.svg"
@@ -73,7 +67,7 @@ export default function Home() {
         <p>名：{onamae.lastName}</p>
       </div>
 
-      <div ref={rootForVue} id="onamae-form" />
+      <div id="onamae-form" />
     </main>
-  );
+  )
 }
